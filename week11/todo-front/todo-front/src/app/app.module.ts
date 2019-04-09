ebed4508d433c4e9e173e0db2c74ab968794ac7a
rruss <sarybayevrruss1@gmail.com>
2019-04-09 23:58:49 +0600

    front added

--- week11/todo-front/todo-front/src/app/app.module.ts
+++ week11/todo-front/todo-front/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProviderService } from './shared/services/provider.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ParentComponent } from './parent/parent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ProviderService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
